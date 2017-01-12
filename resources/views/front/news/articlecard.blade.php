7ad50b5e233ebcbc11041529d052dd7c6faebfe8
Michael Joyner <joyner.michael@gmail.com>
2017-01-12 11:32:07 +0800

    news touches

--- resources/views/front/news/articlecard.blade.php
+++ resources/views/front/news/articlecard.blade.php
@@ -3,7 +3,7 @@
         <img src="{{ $article->titleImg('thumb') }}" alt="{{ $article->title }}">
     </div>
     <div class="article-overview">
-        <h3 class="article-title heavy-heading">{{ $article->title }}</h3>
+        <h3 class="article-title light-heading">{{ $article->title }}</h3>
         <p class="article-published-date light-heading">{{ $article->published_on->toFormattedDateString() }}</p>
         <p class="body-text">{{ $article->description }}</p>
     </div>
--- resources/views/front/news/show.blade.php
+++ resources/views/front/news/show.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <article class="page-section article-show">
-        <h1 class="section-heading">{{ $article->title }}</h1>
+        <h1 class="section-heading article-heading">{{ $article->title }}</h1>
         <p class="article-published-date light-heading">{{ $article->published_on->toFormatteddateString() }}</p>
-        <div class="social-sharing-icons">
+        <div class="social-icon-row">
             <a href="https://twitter.com/home?status={{ urlencode($article->title . ' ' . Request::url()) }}">
                 @include('svgicons.social.twitter')
             </a>
