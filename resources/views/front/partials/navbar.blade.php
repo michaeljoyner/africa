b6f0734c6914e9bd5e1f8d95e403b9b070cd1b89
Michael Joyner <joyner.michael@gmail.com>
2017-01-15 10:46:47 +0800

    dropdown in main navbar

--- resources/views/front/partials/navbar.blade.php
+++ resources/views/front/partials/navbar.blade.php
@@ -10,19 +10,37 @@
         <label for="nav-trigger" class="nav-trigger-icon">@include('svgicons.menu')</label>
         <input type="checkbox" id="nav-trigger">
         <ul class="main-nav-list">
-            <li class="nav-item heavy-text @if(\Illuminate\Support\Facades\Request::path() === '/') active @endif">
+            <li class="nav-item heavy-text @if(Request::path() === '/') active @endif">
                 <a href="/">Home</a>
+                <span class="nav-caret"> @include('svgicons.downcaret')</span>
+                <ul class="secondary-nav-list">
+                    <li class="secondary-nav-item">
+                        <a href="@if(Request::path() !== '/')/@endif#about">About</a>
+                    </li>
+                    <li class="secondary-nav-item">
+                        <a href="@if(Request::path() !== '/')/@endif#team">Team</a>
+                    </li>
+                    <li class="secondary-nav-item">
+                        <a href="@if(Request::path() !== '/')/@endif#partners">Partners</a>
+                    </li>
+                    <li class="secondary-nav-item">
+                        <a href="@if(Request::path() !== '/')/@endif#sponsors">Sponsors</a>
+                    </li>
+                    <li class="secondary-nav-item">
+                        <a href="@if(Request::path() !== '/')/@endif#compliance">Compliance</a>
+                    </li>
+                </ul>
             </li>
-            <li class="nav-item heavy-text @if(starts_with(\Illuminate\Support\Facades\Request::path(), 'expeditions')) active @endif">
+            <li class="nav-item heavy-text @if(starts_with(Request::path(), 'expeditions')) active @endif">
                 <a href="/expeditions">Expeditions</a>
             </li>
-            <li class="nav-item heavy-text @if(starts_with(\Illuminate\Support\Facades\Request::path(), 'gallery')) active @endif">
+            <li class="nav-item heavy-text @if(starts_with(Request::path(), 'gallery')) active @endif">
                 <a href="/gallery">Gallery</a>
             </li>
-            <li class="nav-item heavy-text @if(starts_with(\Illuminate\Support\Facades\Request::path(), 'news')) active @endif">
+            <li class="nav-item heavy-text @if(starts_with(Request::path(), 'news')) active @endif">
                 <a href="/news">News</a>
             </li>
-            <li class="nav-item heavy-text  @if(starts_with(\Illuminate\Support\Facades\Request::path(), 'contact')) active @endif">
+            <li class="nav-item heavy-text  @if(starts_with(Request::path(), 'contact')) active @endif">
                 <a href="/contact">Contact Us</a>
             </li>
         </ul>
