c3878dad248e433cd38e0ac01f800a664d6d45a0
Michael Joyner <joyner.michael@gmail.com>
2017-01-16 09:35:53 +0800

    add favicon files and meta tags

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -8,6 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
     <link rel="stylesheet" href="{{ elixir('css/fapp.css') }}">
     @yield('head')
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="apple-mobile-web-app-title" content="Afrika Freedom Climbers ">
+    <meta name="application-name" content="Afrika Freedom Climbers ">
+    <meta name="theme-color" content="#A62931">
     <script>
         var Laravel = {
             csrfToken: '{{ csrf_token() }}'
